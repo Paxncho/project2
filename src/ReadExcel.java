a01a5afb4e065ae2d3d09aa39fdd8b4e338b7b4b
José Francisco Riffo <jfriffoa@gmail.com>
2016-11-17 02:07:48 -0300

    LeerExcel Arreglado La clase ahora tiene un metodo funcional para leer excels

--- src/ReadExcel.java
+++ src/ReadExcel.java
@@ -1,71 +1,66 @@
-/*import java.io.File;
-import java.util.Scanner;
 
-public class Cargador {
-    
-    public static Sistema readFile ()
-    {
-        try
-        {
-            Sistema sistema = new Sistema ();
-            Scanner scanner = new Scanner (new File(/*ruta de archivo ""));
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+public class ReadExcel{
+    public static void LeerExcel(){
+        try {
+            //Recibir el archivo a leer -Pencho
+            FileInputStream file = new FileInputStream(new File("Horario.xls"));
+
+            //Crear un Workbook con el archivo leido
+            HSSFWorkbook workbook = new HSSFWorkbook(file);
+
+            //Obtener la primera hoja
+            HSSFSheet sheet = workbook.getSheetAt(0);
+
+            //Crear un Iterador que nos recorra las filas mientras existan datos
+            Iterator<Row> rowIterator = sheet.iterator();
+            while(rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+
+                //En cada fila, iteramos sobre cada columna
+                Iterator<Cell> cellIterator = row.cellIterator();
+                    while(cellIterator.hasNext()) {
+
+                    //Obtenemos la celda a revisar
+                    Cell cell = cellIterator.next();
+
+                    //Comparamos el valor que existe en la celda para guardarlo en la variable del mismo tipo
+                    switch(cell.getCellType()) {
+                        case Cell.CELL_TYPE_BOOLEAN:
+                            System.out.print(cell.getBooleanCellValue() + "\t\t");
+                            break;
+                        case Cell.CELL_TYPE_NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "\t\t");
+                            break;
+                        case Cell.CELL_TYPE_STRING:
+                            System.out.print(cell.getStringCellValue() + "\t\t");
+                            break;
+                    }
+                }
+                System.out.println("");
+            }
             
-            scanner.close();
-            return sistema;
-       
-        }
-        catch (Exception e)
-        {
-                System.out.println("No se pudo leer el archivo"+e);
-                return new Sistema();
-        }
+            //Cerramos el archivo al terminar
+            file.close();
+            FileOutputStream out = new FileOutputStream(new File("C:\\test.xls"));
+            workbook.write(out);
+            out.close();
+
+    } catch (FileNotFoundException e) {
+            e.printStackTrace();
+    } catch (IOException e) {
+            e.printStackTrace();
     }
-}*/
 
-import java.io.*; 
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-
-public class ReadExcel { 
-    private void leerArchivoExcel(String archivoDestino) { 
-
-        try { 
-            Workbook archivoExcel = Workbook.getWorkbook(new File( archivoDestino)); 
-            System.out.println("Número de Hojas\t" + archivoExcel.getNumberOfSheets()); 
-            for (int sheetNo = 0; sheetNo < archivoExcel.getNumberOfSheets(); sheetNo++) // Recorre 
-            // cada    
-            // hoja                                                                                                                                                       
-            { 
-                Sheet hoja = archivoExcel.getSheet(sheetNo); 
-                int numColumnas = hoja.getColumns(); 
-                int numFilas = hoja.getRows(); 
-                String data; 
-                System.out.println("Nombre de la Hoja\t" 
-                + archivoExcel.getSheet(sheetNo).getName()); 
-                for (int fila = 0; fila < numFilas; fila++) { // Recorre cada 
-                    // fila de la 
-                    // hoja 
-                    for (int columna = 0; columna < numColumnas; columna++) { // Recorre                                                                                
-                        // cada                                                                                
-                        // columna                                                                            
-                        // de                                                                                
-                        // la                                                                                
-                        // fila 
-                        data = hoja.getCell(columna, fila).getContents(); 
-                        System.out.print(data + " "); 
-
-                    } 
-                    System.out.println("\n"); 
-                } 
-            } 
-        } catch (Exception ioe) { 
-            ioe.printStackTrace(); 
-        } 
-
-    } 
-
-    /*public static void main(String arg[]) { 
-        ReadExcel excel = new ReadExcel(); 
-        excel.leerArchivoExcel("archivoPrueba.xls"); 
-    } 
-}*/
+    }
+}
