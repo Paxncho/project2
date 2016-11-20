b74aac60d5833caaee01bf2623dd47cab9e0d041
Luxho97 <l.seba.aburto@gmail.com>
2016-11-19 23:06:29 -0300

    Mi hermosisimo Main
    <3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,144 @@
 
 public class Main {
 
+    private static Lector donLector = new Lector();
+    private static Sistema sistema = new Sistema();
+    
     public static void main(String[] args) {
+        int opcion;
+        
+        do{
+            Menu.mostrarMenuPrincipal();
+            opcion = donLector.leerOpcion("Ingresa tu opcion: ", 0, 4);
+
+            switch (opcion)
+            {
+                case 1:
+                    Menu.mostrarMenuIngresarRemover("Sala");
+                    opcion = donLector.leerOpcion("Ingresa tu opcion: ",0, 2);
+                    switch (opcion)
+                    {
+                        case 1:
+                            Ingresar_Sala();
+                            break;
+                        case 2:
+                            Remover_Sala();
+                            break;
+                        case 0:
+                            opcion=7;
+                            break;
+                    }
+                    break;
+                case 2:
+                    Menu.mostrarMenuIngresarRemover("Laboratorio");
+                    opcion = donLector.leerOpcion("Ingresa tu opcion: ",0, 2);
+                    switch (opcion)
+                    {
+                        case 1:
+                            Ingresar_Laboratorio();
+                            break;
+                        case 2:
+                            Remover_Laboratorio();
+                            break;
+                        case 0:
+                            opcion=7;
+                            break;
+                    }
+                    break;
+                case 3:
+                    CargarDemanda_Aula();
+                    break;
+                case 4:
+                    Menu.mostrarMenu1();
+                    opcion = donLector.leerOpcion ("Ingresa tu opcion: ",0,2);
+                    switch (opcion)
+                    {
+                        case 1:
+                            VerListadoDemanda_Aula();
+                            break;
+                        case 2:
+                            Menu.mostrarMenu2();
+                            opcion = donLector.leerOpcion ("Ingresa tu opcion: ",0,2);
+                            switch (opcion)
+                            {
+                                case 1:
+                                    Menu.mostrarMenuSalaLaboratorio();
+                                    opcion = donLector.leerOpcion ("Ingresa tu opcion :",0,1);
+                                    switch (opcion)
+                                    {
+                                        case 1:
+                                            Asignar_Sala();
+                                            break;
+                                        case 2:
+                                            Asignar_Laboratorio();
+                                            break;
+                                        case 0:
+                                            opcion = 7;
+                                            break;
+                                    }
+                                    
+                                    break;
+                                case 2:
+                                    break;
+                                case 0:
+                                    break;
+                                    
+                            }
+                            break;
+                        case 0:
+                            Menu.mostrarMenuPrincipal();
+                            opcion=7;
+                            break;
+                    }
+                    break;
+                case 0:
+                    System.out.println("Gracias por usar el programa!!");
+                    System.out.println("---FIN---");
+                    break;
+            }
+            
+        }while(opcion != 0);
 
     }
+    private static void CargarDemanda(){
+        
+    }
+    private static void ResolverDemanda (){
+    
+    }
+    private static void Ingresar_Sala(){
+        String nombre;
+        int capacidadAlumnos;
+        nombre =donLector.leerLinea("Ingresa el nombre");
+        capacidadAlumnos = donLector.leerNumeroEntero("Ingresa la Capacidad de Alumnos de la Sala");
+        
+        
+    }
+    
+    private static void Remover_Sala(){
+        
+    }
+    
+    private static void Ingresar_Laboratorio(){
+        
+    }
+    
+    private static void Remover_Laboratorio(){
+        
+    }
+    private static void CargarDemanda_Aula (){
+        
+    }
+    private static void VerListadoDemanda_Aula(){
+        
+    }
+    private static void Asignar_Sala(){
+        
+    }
+    private static void Asignar_Laboratorio(){
+        
+    }
+
+    
 
 }
--- src/Menu.java
+++ src/Menu.java
@@ -3,35 +3,27 @@
 public class Menu
 {
     
-   static public void mostrarMenu()
+   static public void mostrarMenuPrincipal()
    {
        /*
       el Menú Principal (Ingresar/Remover Sala, Ingresar/Remover Laboratorio, Cargar Demanda, Resolver Demanda, Volver),
        Resolver Demanda (Ver listado de demandas cumplidas,Seleccionar actividad, Volver) y Seleccionar Actividad 
        (Asignar sala/laboratorio, Remover sala/laboratorio asignado, Volver).*/
-        System.out.println ("\n");
         System.out.println ("Menú");
         System.out.println ("\n");
         System.out.println ("1) Ingresar/Remover Sala");
-        System.out.println ("\n");
         System.out.println ("2) Ingresar/Remover Laboratorio");
-        System.out.println ("\n");
         System.out.println ("3) Cargar Demanda");
-        System.out.println ("\n");
         System.out.println ("4) Resolver Demanda");
-        System.out.println ("\n");
-        System.out.println ("0) Volver");
+        System.out.println ("0) Salir");
         System.out.println ("\n");
    }
-   
+  
    static public void mostrarMenu1()
    {
         System.out.println ("Resolver Demanda");
-        System.out.println ("\n");
         System.out.println ("1) Ver Listado de Demandas Cumplidas");
-        System.out.println ("\n");
         System.out.println ("2) Seleccionar Actividad");
-        System.out.println ("\n");
         System.out.println ("0) Volver");
         System.out.println ("\n");
    }
@@ -41,15 +33,29 @@ public class Menu
         System.out.println ("Seleccionar Actividad");
         System.out.println ("\n");
         System.out.println ("1) Asignar Sala/Laboratorio");
-        System.out.println ("\n");
         System.out.println ("2) Remover Sala/Laboratorio Asignado");
-        System.out.println ("\n");
         System.out.println ("0) Volver");
         System.out.println ("\n");
 
    }
    
-
+   static public void mostrarMenuIngresarRemover(String aula)
+   {
+       System.out.println ("Ingresar o Remover " + aula);
+       System.out.println ("1) Ingresar " + aula);
+       System.out.println ("2) Remover " + aula);
+       System.out.println ("0) Volver");
+       System.out.println ("\n");
+   }
+   
+   static public void mostrarMenuSalaLaboratorio ()
+   {
+       System.out.println("Sala o Laboratorio ");
+       System.out.println("1) Sala ");
+       System.out.println("2) Laboratorio ");
+       System.out.println("0) Volver");
+       System.out.println("\n");
+   }
    
 }
 
